84f8d730c7c45724b60c1f025dc5a03d1486f6e8
Deepika <149404971+DeepikaH2005@users.noreply.github.com>
2024-10-04 22:30:17 +0530

    Add files via upload

---
+++ To-do.js
@@ -0,0 +1,46 @@
+console.log("+-------+------MENU-------+--------+");
+console.log("|S.no   |Name             |Prize   |");
+console.log("+-------+-----------------+--------+");
+console.log("|1.     |Strawberry       |Rs.110  |");
+console.log("+-------+-----------------+--------+");
+console.log("|2.     |Vanilla          |Rs.120  |");
+console.log("+-------+-----------------+--------+");
+console.log("|3.     |Chocolate        |Rs.120  |");
+console.log("+-------+-----------------+--------+");
+console.log("|4.     |Mango            |Rs.150  |");
+console.log("+-------+-----------------+--------+\n\n");
+
+n=prompt("Enter the S.no (5 to stop the Order) :")
+n=Number(n)
+let s=0
+let  f=0
+while (f==0){
+    switch(n){
+        case 1:
+            s=s+110
+            break;
+        case 2:
+            s=s+120
+            break;
+        case 3:
+            s=s+120
+            break;
+        case 4:
+            s=s+150
+            break;
+        case 5:
+            f=1
+            break;
+        default :
+        console.log("Enter the Correct choice!!!")
+    }
+    if (f==0){
+        console.log("Current Total  : Rs.",s)
+        n=prompt("Enter the S.no (5 to stop the Order) :")
+        n=Number(n)
+    }
+}
+
+
+    console.log("\n\n\mFinal  prize : Rs.",s);
+   
